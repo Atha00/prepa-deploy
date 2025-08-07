f8150910303a71d81a880228172936b69438b646
Michelot Alexis <amichelot@club-internet.fr>
2025-08-07 21:34:12 +0200

    added PORT

--- index.js
+++ index.js
@@ -96,6 +96,6 @@ app.all(/.*/, (req, res) => {
   return res.status(404).json("Not found");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server started 🔥🔥🔥");
 });
